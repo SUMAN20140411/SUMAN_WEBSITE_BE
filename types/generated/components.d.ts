76496f20e3503acbcf4a3c1385141a645e553206
gideonfrans22 <gideonfrans22@gmail.com>
2026-03-06 13:27:06 +0900

    first commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,104 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface HomepageBubbleKeys extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_bubble_keys';
+  info: {
+    displayName: 'bubbleKeys';
+    icon: 'information';
+  };
+  attributes: {};
+}
+
+export interface HomepageSection2 extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_section2s';
+  info: {
+    displayName: 'section2';
+    icon: 'apps';
+  };
+  attributes: {
+    buttonLabel: Schema.Attribute.String;
+    description: Schema.Attribute.Text;
+    hero: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    keyword1: Schema.Attribute.Component<'homepage.bubble-keys', true>;
+    subTitle: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SharedMedia extends Struct.ComponentSchema {
+  collectionName: 'components_shared_media';
+  info: {
+    displayName: 'Media';
+    icon: 'file-video';
+  };
+  attributes: {
+    file: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+  };
+}
+
+export interface SharedQuote extends Struct.ComponentSchema {
+  collectionName: 'components_shared_quotes';
+  info: {
+    displayName: 'Quote';
+    icon: 'indent';
+  };
+  attributes: {
+    body: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SharedRichText extends Struct.ComponentSchema {
+  collectionName: 'components_shared_rich_texts';
+  info: {
+    description: '';
+    displayName: 'Rich text';
+    icon: 'align-justify';
+  };
+  attributes: {
+    body: Schema.Attribute.RichText;
+  };
+}
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    description: '';
+    displayName: 'Seo';
+    icon: 'allergies';
+    name: 'Seo';
+  };
+  attributes: {
+    metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
+    metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
+    shareImage: Schema.Attribute.Media<
+      'images' | 'videos' | 'audios' | 'files'
+    >;
+  };
+}
+
+export interface SharedSlider extends Struct.ComponentSchema {
+  collectionName: 'components_shared_sliders';
+  info: {
+    description: '';
+    displayName: 'Slider';
+    icon: 'address-book';
+  };
+  attributes: {
+    files: Schema.Attribute.Media<'images', true>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'homepage.bubble-keys': HomepageBubbleKeys;
+      'homepage.section2': HomepageSection2;
+      'shared.media': SharedMedia;
+      'shared.quote': SharedQuote;
+      'shared.rich-text': SharedRichText;
+      'shared.seo': SharedSeo;
+      'shared.slider': SharedSlider;
+    }
+  }
+}
