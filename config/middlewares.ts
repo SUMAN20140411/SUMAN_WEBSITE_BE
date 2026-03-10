90f77571052355ac8785a3e820084f65e4666125
gideonfrans22 <gideonfrans22>
2026-03-10 11:11:06 +0900

    Refactor middleware configuration for improved security policies. Updated content security policy directives to include new sources for images, scripts, and media, enhancing support for Cloudinary and map plugins. Cleaned up formatting for better readability.

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,55 +1,69 @@
-import type {Core}
-from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
 
 const config: Core.Config.Middlewares = [
-    'strapi::logger',
-    'strapi::errors',
-    {
-        name: 'strapi::security',
-        config: {
-            contentSecurityPolicy: {
-                useDefaults: true,
-                directives: {
-                    'connect-src': [
-                        "'self'", 'https:'
-                    ],
-                    'img-src': [
-                        "'self'",
-                        'data:',
-                        'blob:',
-                        'https://market-assets.strapi.io',
-                        'https://console.cloudinary.com',
-                        'https://res.cloudinary.com',
-                    ],
-                    'script-src': [
-                        "'self'",
-                        'example.com',
-                        'https://media-library.cloudinary.com',
-                        'https://upload-widget.cloudinary.com',
-                        'https://console.cloudinary.com',
-                    ],
-                    'media-src': [
-                        "'self'",
-                        'data:',
-                        'blob:',
-                        'https://console.cloudinary.com',
-                        'https://res.cloudinary.com',
-                    ],
-                    'frame-src': [
-                        "'self'", 'https://media-library.cloudinary.com', 'https://upload-widget.cloudinary.com', 'https://console.cloudinary.com',
-                    ],
-                    upgradeInsecureRequests: null
-                }
-            }
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://market-assets.strapi.io",
+            // cloudinary related
+            "https://console.cloudinary.com",
+            "https://res.cloudinary.com",
+            // map plugin related
+            "https://*.basemaps.cartocdn.com",
+            "https://*.tile.openstreetmap.org",
+            "https://unpkg.com/leaflet@1.9.4/dist/images/"
+          ],
+          "script-src": [
+            "'self'",
+            "unsafe-inline",
+            "example.com",
+            // cloudinary related
+            "https://media-library.cloudinary.com",
+            "https://upload-widget.cloudinary.com",
+            "https://console.cloudinary.com",
+            // map plugin related
+            "https://*.basemaps.cartocdn.com"
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            // cloudinary related
+            "https://console.cloudinary.com",
+            "https://res.cloudinary.com",
+            // map plugin related
+            "https://*.basemaps.cartocdn.com",
+            "https://tile.openstreetmap.org",
+            "https://*.tile.openstreetmap.org"
+          ],
+          "frame-src": [
+            "'self'",
+            "https://media-library.cloudinary.com",
+            "https://upload-widget.cloudinary.com",
+            "https://console.cloudinary.com"
+          ],
+          upgradeInsecureRequests: null
         }
-    },
-    'strapi::cors',
-    'strapi::poweredBy',
-    'strapi::query',
-    'strapi::body',
-    'strapi::session',
-    'strapi::favicon',
-    'strapi::public',
+      }
+    }
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public"
 ];
 
 export default config;
